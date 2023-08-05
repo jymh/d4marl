aedbc467a44a30c56b89e1a332a5dceb51485a9a
jymh <760805792@qq.com>
2023-08-05 18:51:15 +0800

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,24 @@ author = 'offline MARL team'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.extlinks',
+    'recommonmark',
+    'sphinx_markdown_tables',
+    'sphinx_design',
+    'sphinx_copybutton',
+    'sphinx_autodoc_typehints',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
